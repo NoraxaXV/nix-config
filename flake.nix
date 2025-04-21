1a07264dcda70aaa5745d9f59c416ea7ffd8a1dc
NoraxaXV <kurororbm@gmail.com>
2025-04-21 17:52:57 -0400

    fixed syntax error

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Excalibur Config";
 
   inputs = {
-    nixpkgs. url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
